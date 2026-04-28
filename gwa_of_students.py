817d5804eab28cd77822a0012e4e77ef0d2974d1
jbboncay17 <jbboncay1@gmail.com>
2026-04-29 07:58:43 +0800

    initial commit

---
+++ gwa_of_students.py
@@ -0,0 +1,7 @@
+class StudentGWA:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def get_highest(self):
+        highest_name = ""
+        highest_gwa = float('inf')
